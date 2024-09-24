305e9a65d41d35186d6339b22cd011b59ec9cc73
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2024-09-24 14:05:05 +0000

    [feat] Adding Relatorios page
    Signed-off-by: GitHub <noreply@github.com>

--- src/pages/Relatorio/Relatorio.tsx
+++ src/pages/Relatorio/Relatorio.tsx
@@ -1,9 +1,15 @@
 import "../../style/Relatorio/Relatorio.css";
 
-function Relatorio () {
+function Relatorio() {
     return (
         <div className="looker-container">
-            <iframe src="https://lookerstudio.google.com/embed/reporting/eb34d02c-012d-4a85-8161-06e01b91890b/page/p_ipv5f677kd" frameborder="0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+            <iframe
+                src="https://lookerstudio.google.com/embed/reporting/eb34d02c-012d-4a85-8161-06e01b91890b/page/p_ipv5f677kd"
+                style={{ border: "none" }}
+                allowFullScreen
+                sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+                title="Looker Studio Report"
+            ></iframe>
         </div>
     );
 }
